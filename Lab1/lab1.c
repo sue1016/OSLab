c3c3a0ea9be180fdedc27ecc75b40f5d46c3e32d
BigbrotherSue <skx15826273720@gmail.com>
2018-03-19 14:40:30 +0800

    have writen all the code of lab1.c, but have not tested

--- Lab1/lab1.c
+++ Lab1/lab1.c
@@ -4,13 +4,38 @@
 
 int main()
 {
-    pid_t cpid1;
+    pid_t cpid1, cpid2;
     cpid1 = fork();
+    if(-1 == cpid1)
+    {
+        printf("failed to fork child process1");
+    }
+    else if(0 == cpid1)
+    {
+        printf("b");
 
+         //This is the child process1, and it has to return 0,
+         // otherwise it will fork the second child process too
+        
+         return 0;       
+     }
+    
+    //only the father process can fork the second process
+   
+    cpid2 = fork();
+    if(-1 == cpid2)
+    {
+       printf("failed to fork child process2");
+    }
+    else if(0 == cpid2)
+    {
+        print("c");
+    }
+    else
+    {
+        printf("a"); 
+    }
+    
     return 0;
-
-
-
-
-
+ 
 }
