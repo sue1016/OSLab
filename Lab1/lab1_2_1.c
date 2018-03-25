e358fe78e0eb00f00279c9207c59b3b2cc96fb83
BigbrotherSue <skx15826273720@gmail.com>
2018-03-25 22:11:18 +0800

    debug for lab1 and lab2

--- Lab1/lab1_2_1.c
+++ Lab1/lab1_2_1.c
@@ -14,7 +14,7 @@ int main()
     } 
     else if(pid == 0)
     {
-       if((ret = execlp("./lab1_1","lab1_1",NULL)) < 0)
+       if((ret = execlp("./lab1_1_2","lab1_1_2",NULL)) < 0)
        {
            printf("execlp error");
        }    
