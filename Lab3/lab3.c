5eb0881cb52bed281c9a1296afa49432ef15ec45
sue1016 <15826273720@163.com>
2018-04-30 21:06:48 +0800

    lab3 report done

--- Lab3/lab3.c
+++ Lab3/lab3.c
@@ -123,6 +123,9 @@ int main()
 
 	pthread_join(prod_t, &retval );
 	pthread_join(cons_t, &retval );
-	
+        //代码中如果没有pthread_join主线程会很快结束从而使整个进程结束
+	//从而使创建的线程没有机会开始执行就结束了。加入pthread_join后，
+	//主线程会一直等待直到等待的线程结束自己才结束，
+	//使创建的线程有机会执行。	
 	return 0;
 }
